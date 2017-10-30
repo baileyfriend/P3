e1ed299983c66f0bc18920bcdfbde4f959dfbf13
Madison Brooks <brooksma@mail.gvsu.edu>
2017-10-30 01:07:46 -0400

    Add files via upload

--- route.c
+++ route.c
@@ -16,6 +16,25 @@
 #include <netinet/ip.h>
 #include <string.h>
 
+//struct ether_header
+//struct iphdr 
+//struct arphdr
+  //{
+    //unsigned short int ar_hrd;		/* Format of hardware address.  */
+ //   unsigned short int ar_pro;		/* Format of protocol address.  */
+  //  unsigned char ar_hln;		/* Length of hardware address.  */
+  //  unsigned char ar_pln;		/* Length of protocol address.  */
+   // unsigned short int ar_op;		/* ARP opcode (command).  */
+//#if 0
+    /* Ethernet looks like this : This bit is variable sized
+       however...  */
+  //  unsigned char __ar_sha[ETH_ALEN];	/* Sender hardware address.  */
+    //unsigned char __ar_sip[4];		/* Sender IP address.  */
+  //  unsigned char __ar_tha[ETH_ALEN];	/* Target hardware address.  */
+    //unsigned char __ar_tip[4];		/* Target IP address.  */
+//#endif
+ // };
+//struct iphdr
 
 //end of added code
 
